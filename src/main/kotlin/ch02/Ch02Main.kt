3a3aa58f2270229e6eb4c6ec9a139f86805132bd
baekho <klyhyeon@gmail.com>
2024-11-10 23:09:27 +0900

    feat: Enum and when

--- src/main/kotlin/ch02/Ch02Main.kt
+++ src/main/kotlin/ch02/Ch02Main.kt
@@ -3,40 +3,41 @@ package ch02
 import ch02.createRandomRectangle
 
 fun main() {
-  println(Color.RED.rgb())
-
-  println(max(1, 2))
-
-  //다른 패키지 함수 import 하기
-  createRandomRectangle().isSquare
-
-  val list = arrayListOf("10", "11", "1001")
-  for ((index, value) in list.withIndex()) {
-    println("$index: $value")
-  }
-
-  // val 할당하기
-  val str: String
-  if (list.isNullOrEmpty()) {
-    str = "is empty"
-  } else {
-    str = "not empty"
-  }
-
-  println(str)
-
-  val person = Person(
-    name = "유현",
-    false,
-  )
-  println("name: ${person.name}")
-  println("is married: ${person.isMarried}")
-
-  val notMarriedCouples = listOf(Person("yuhyeon", false), Person("jiwoo", false))
-
-  val byLastName = notMarriedCouples.associateBy { it.name }
-
-  println(byLastName)
+    println(eval(Sum(Sum(Num(1), Num(2)), Num(4))))
+//  println(Color.RED.rgb())
+//
+//  println(max(1, 2))
+//
+//  //다른 패키지 함수 import 하기
+//  createRandomRectangle().isSquare
+//
+//  val list = arrayListOf("10", "11", "1001")
+//  for ((index, value) in list.withIndex()) {
+//    println("$index: $value")
+//  }
+//
+//  // val 할당하기
+//  val str: String
+//  if (list.isNullOrEmpty()) {
+//    str = "is empty"
+//  } else {
+//    str = "not empty"
+//  }
+//
+//  println(str)
+//
+//  val person = Person(
+//    name = "유현",
+//    false,
+//  )
+//  println("name: ${person.name}")
+//  println("is married: ${person.isMarried}")
+//
+//  val notMarriedCouples = listOf(Person("yuhyeon", false), Person("jiwoo", false))
+//
+//  val byLastName = notMarriedCouples.associateBy { it.name }
+//
+//  println(byLastName)
 }
 
 fun max(a: Int, b: Int) = if (a > b) a else b
@@ -44,18 +45,27 @@ fun max(a: Int, b: Int) = if (a > b) a else b
 interface Expr
 
 class Sum(
-  val right: Int,
-  val left: Int
+    val right: Expr,
+    val left: Expr,
 ) : Expr
 
 class Num(
-  val value: Int
+    val value: Int
 ) : Expr
 
-fun eval(e: Expr): Int =
-  when (e) {
-    is Num -> e.value
-    is Sum -> e.right
-    else -> throw IllegalArgumentException("Unknown expressions!")
-  }
+fun eval(e: Expr): Int {
+    // java style code
+//    if (e is Num) {
+//        return e.value
+//    }
+//    if (e is Sum) {
+//        return eval(e.right) + eval(e.left)
+//    }
+//    throw IllegalArgumentException("Unknown expression")
+    when (e) {
+        is Num -> return e.value
+        is Sum -> return eval(e.right) + eval(e.left)
+        else -> throw IllegalArgumentException("Unknown expression")
+    }
+}
 
